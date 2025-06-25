4d1472d81cd42c21301117fd46457d1e8e907ce4
Matheus-Freitas0 <moncaomatheus@gmail.com>
2025-06-24 21:13:41 -0300

    Finished Poc-Swagger

--- src/main/java/com/poc/swagger/controller/UserController.java
+++ src/main/java/com/poc/swagger/controller/UserController.java
@@ -2,6 +2,7 @@ package com.poc.swagger.controller;
 
 import com.poc.swagger.domain.User;
 import com.poc.swagger.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,27 +17,30 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(description = "Buscar um usuário pelo ID")
     @GetMapping("/user/{id}")
     public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.getUserName(id));
     }
-
+    @Operation(description = "Listar todos os usuários")
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok().body(userService.getAllUsers());
     }
-
+    @Operation(description = "Deletar um usuário")
     @DeleteMapping("/user/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Criar novo usuário")
     @PostMapping("/user")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return ResponseEntity.ok().body(userService.saveUser(user));
     }
 
+    @Operation(description = "Atualizar dados do usuário")
     @PutMapping("/user/update/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
         return ResponseEntity.ok().body(userService.updateUser(userDetails, id));
